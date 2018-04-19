5ba3fd8af3640a2fb83d5416adb2aa1de570e4b3
Jacob Versaggi <jcv42.phil@gmail.com>
2018-04-19 15:41:49 -0400

    Revert "First commit"
    This reverts commit 3d072051fc1f865caaba7a28458b3b0392b3e48c.

--- savedHomesXlxsDl.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-r = requests.get("http://docket.philalegal.org/data/foreclosure-addresses.xlsx")
-print("Got request")
-f = open('saved_homes.xlsx','wb')
-print("Made file")
-f.write(r.content)
-print("Done excel write")
